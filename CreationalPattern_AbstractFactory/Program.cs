0bcac16e86f7344469e4cf50f5211723cf2b9b53
deoresulakshana <deoresulakshana@gmail.com>
2023-10-27 11:12:33 +0530

    non thread safe code

--- CreationalPattern_AbstractFactory/Program.cs
+++ CreationalPattern_AbstractFactory/Program.cs
@@ -10,19 +10,30 @@ namespace CreationalPattern_AbstractFactory
     {
         static void Main(string[] args)
         {
-
-            
-
-
-            UserLogin login = UserLogin.CreateUser();
-            login.Userid = "Gauri";
-            login.Password = "password";
+            UserLogin login1= UserLogin.CreateUserUsingMutex();
+            login1.Userid = "Amita";
+            login1.Password = "Passwor";
+           Console.WriteLine(login1.Userid);
+            Console.WriteLine(login1.Password);
             Console.WriteLine("User created successfully");
 
-            UserLogin login2 = UserLogin.CreateUser();
-            login2.Userid = "Sima";
-            login2.Password = "password";
-            Console.WriteLine("User created successfully");
+
+            UserLogin login2 = UserLogin.CreateUserUsingMutex();
+
+
+
+
+
+
+            //UserLogin login = UserLogin.CreateUser();
+            //login.Userid = "Gauri";
+            //login.Password = "password";
+            //Console.WriteLine("User created successfully");
+
+            //UserLogin login2 = UserLogin.CreateUser();
+            //login2.Userid = "Sima";
+            //login2.Password = "password";
+            //Console.WriteLine("User created successfully");
 
             Console.ReadLine();
         }
--- CreationalPattern_AbstractFactory/UserLogin.cs
+++ CreationalPattern_AbstractFactory/UserLogin.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace CreationalPattern_AbstractFactory
@@ -13,15 +14,36 @@ namespace CreationalPattern_AbstractFactory
         public string Userid { get; set; }
         public string Password { get; set; }
 
+      //  public  static UserLogin Usercreation { get { return instance; } private set { instance = value; } }
+
         private UserLogin()
         {
             
         }
-        public static UserLogin CreateUser()
+
+
+        public static UserLogin CreateUserUsingMutex()
+        {
+            
+                        if (instance!=null)
+                    {
+                        Console.WriteLine("User already created");
+                    }
+               else
+                {
+                    instance = new UserLogin();
+                }
+                return instance;
+            
+             
+        }
+
+
+            public static UserLogin CreateUser()
         {
             lock (_lockobject)
             {
-                if (UserLogin.instance==null)
+                if (UserLogin.instance == null)
                 {
                     instance = new UserLogin();
                 }
